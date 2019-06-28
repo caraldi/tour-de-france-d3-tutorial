d66aaaee4c838f004941dce26b0aed98761e47ea
Araldi, Caitlin (ETW - FLEX) <Caitlin.Araldi@nike.com>
2019-06-28 16:27:33 -0700

    Removed boilerplate code from App and Home components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Tour de France 1903-2016"/>
   </div>
 </template>
 
