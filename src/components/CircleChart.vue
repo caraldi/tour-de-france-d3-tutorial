75c9717e6f1d9b2469dcdfd4dff3e5eba3d79305
Araldi, Caitlin (ETW - FLEX) <Caitlin.Araldi@nike.com>
2019-07-01 12:30:01 -0700

    Created presentational chart component

---
+++ src/components/CircleChart.vue
@@ -0,0 +1,22 @@
+<!--
+  From the Flowing Data tutorial "Getting Started with D3.js" by Peter Cook
+  Source: https://flowingdata.com/2019/06/18/getting-started-with-d3/
+-->
+
+<template>
+  <div class="chart-wrapper">
+    <svg
+      width="800"
+      height="1000"
+    >
+      <g class="chart chart--circle">
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CircleChart'
+}
+</script>
