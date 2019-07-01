0ae42ae22de1e3b0f58b3aad805d079d180e1431
Araldi, Caitlin (ETW - FLEX) <Caitlin.Araldi@nike.com>
2019-07-01 12:31:24 -0700

    Added chart component with data to view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <CircleChart/>
   </div>
 </template>
 
 <script>
-// import * as d3 from 'd3'
 import * as types from '@/store/types'
+import CircleChart from '@/components/CircleChart'
 
 export default {
   name: 'HelloWorld',
+  components: {
+    CircleChart
+  },
   props: {
     msg: String
   },
   created () {
-    return this.$store.dispatch(types.FETCH_DATA)
+    return this.$store.dispatch(types.FETCH_CHART_DATA)
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
